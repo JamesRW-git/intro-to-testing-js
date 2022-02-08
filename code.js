d2faa5de8d24aa0f58a64e74b4c7591b6bfe7af0
James Woodward <jamesrobertwoodward@gmail.com>
2022-02-08 15:12:18 -0600

    update intro-to-testing-js

--- code.js
+++ code.js
@@ -3,6 +3,10 @@
 //    return "Hello, World!";
 //}
 
-function sayHello() {
-    return "";
+function sayHello(input) {
+    if(input === "Alex") {
+        return "Hello, Alex!"
+    } else {
+        return "Hello, Jane!";
+    }
 }
